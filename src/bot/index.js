19c31bfa96e3c83c1592e7572179145fcd2e64d2
Kirill Shapoval <kirill.shapoval@gmail.com>
2024-07-10 18:53:13 +0300

    Refactor; Add basic telegram bot functionality;

---
+++ src/bot/index.js
@@ -0,0 +1,88 @@
+import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
+
+import ChatGPT from '../lib/index.js';
+
+dotenv.config();
+
+class BotService {
+  constructor() {
+    this._bot = new TelegramBot(process.env.TG_BOT_TOKEN, { polling: true });
+    this._openChats = {};
+    this._inactivityTimeout = 300000; // 5 minutes
+  }
+
+  async start() {
+    // Init commands
+    await this._bot.setMyCommands([
+      { command: 'start', description: 'Start a new chat' },
+    ]);
+
+    // Setup inactivity tracking
+    this._resetInactivityTimer(); // Bot will stop polling after timeout if inactive
+
+    // Incoming messages handler (only text)
+    this._bot.on('text', async (msg) => {
+      // Update activity
+      this._resetInactivityTimer();
+
+      const text = msg.text;
+      const chatId = msg.chat.id;
+
+      // Init new chat
+      if (text === '/start') {
+        // Create new chat, save it by user chatId
+        this._openChats[chatId] = { gpt: new ChatGPT(), isProcessing: false };
+        await this._bot.sendMessage(chatId, 'How can I help you?');
+        return;
+      }
+
+      // /start command is required
+      if (!Object.hasOwn(this._openChats, chatId)) {
+        await this._bot.sendMessage(
+          chatId,
+          'You should start a new chat first (send /start command)',
+        );
+        return;
+      }
+
+      // Prevent from flooding with requests to gpt
+      if (this._openChats[chatId].isProcessing) {
+        await this._bot.sendMessage(chatId, 'Please wait...');
+      }
+
+      // Main flow
+      this._openChats[chatId].isProcessing = true;
+
+      try {
+        await this._bot.sendMessage(chatId, '...');
+        const response = await this._openChats[chatId].gpt.askQuestion(text);
+        await this._bot.sendMessage(chatId, response);
+      } catch (error) {
+        console.error(error);
+        await this._bot.sendMessage(chatId, "Can't process this request");
+      } finally {
+        this._openChats[chatId].isProcessing = false;
+      }
+    });
+  }
+
+  stop() {
+    this._bot.stopPolling();
+    console.log('Bot stopped due to inactivity.');
+  }
+
+  _resetInactivityTimer() {
+    if (this._inactivityTimer) {
+      clearTimeout(this._inactivityTimer);
+    }
+    this._inactivityTimer = setTimeout(() => {
+      this.stop();
+    }, this._inactivityTimeout);
+  }
+}
+
+const botService = new BotService();
+botService.start();
+
+// export default BotService;
